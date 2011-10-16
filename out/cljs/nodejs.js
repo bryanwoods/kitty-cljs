08b232adcc2377a53b3c04e0cec629dbe7b578c6
Bryan Woods <bryanwoods4e@gmail.com>
2011-10-16 16:53:17 -0400

    Adds out/ to gitignore

--- out/cljs/nodejs.js
+++
@@ -1,5 +0,0 @@
-goog.provide('cljs.nodejs');
-goog.require('cljs.core');
-cljs.nodejs.require = require;
-cljs.nodejs.process = process;
-cljs.core.string_print = cljs.nodejs.require.call(null,"sys").print;
